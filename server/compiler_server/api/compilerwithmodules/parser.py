8592134ec4b299de36230227554cee6aa21a6209
pedrosavio <pedro_savio777@hotmail.com>
2025-04-06 18:59:01 -0300

    feat: create new parse

--- server/compiler_server/api/compilerwithmodules/parser.py
+++ server/compiler_server/api/compilerwithmodules/parser.py
@@ -24,6 +24,137 @@ PARSER_DISPATCH = {
     'ASSIGN': parse_assign
 }
 
+
+# Funções de encaixe na AST
+def handle_module_start(node, ast, module_stack, block_stack):
+    ast.append(node)
+    module_stack.append(node)
+    logger.debug("[Parser] Adicionado MODULE à AST: %s", str(node))
+
+
+def handle_module_end(node, ast, module_stack, block_stack):
+    if module_stack:
+        module_stack.pop()
+    if block_stack:
+        block_stack.clear()
+    logger.debug("[Parser] MODULE finalizado, AST atual: %s", [str(n) for n in ast])
+
+
+def handle_always_posedge(node, ast, module_stack, block_stack):
+    if module_stack:
+        module_stack[-1].children.append(node)
+        block_stack.append(node)  # Inicia o bloco
+        logger.debug("[Parser] Aninhado ALWAYS_POSEDGE no MODULE e block_stack: %s", str(node))
+
+
+def handle_if(node, ast, module_stack, block_stack):
+    if block_stack:
+        block_stack[-1].children.append(node)
+        block_stack.append(node)  # Empilha o IF como bloco ativo
+        logger.debug("[Parser] Aninhado IF no bloco ativo e empilhado: %s", str(node))
+
+
+def handle_else(node, ast, module_stack, block_stack):
+    if block_stack and block_stack[-1].type == 'IF':
+        block_stack.pop()  # Fecha o IF
+    if block_stack:
+        block_stack[-1].children.append(node)
+        block_stack.append(node)  # Empilha o ELSE como bloco ativo
+        logger.debug("[Parser] Aninhado ELSE no bloco ativo e empilhado: %s", str(node))
+
+
+def handle_assign_print(node, ast, module_stack, block_stack):
+    if block_stack:
+        block_stack[-1].children.append(node)
+        logger.debug("[Parser] Aninhado %s no bloco ativo: %s", node.type, str(node))
+    elif module_stack:
+        module_stack[-1].children.append(node)
+        logger.debug("[Parser] Aninhado %s no MODULE: %s", node.type, str(node))
+    else:
+        ast.append(node)
+        logger.debug("[Parser] Adicionado %s à AST (fora de módulo): %s", node.type, str(node))
+
+
+def handle_var_decl(node, ast, module_stack, block_stack):
+    if block_stack:
+        block_stack[-1].children.append(node)
+        logger.debug("[Parser] Aninhado VAR_DECL no bloco ativo: %s", str(node))
+    elif module_stack:
+        module_stack[-1].children.append(node)
+        logger.debug("[Parser] Aninhado VAR_DECL no MODULE: %s", str(node))
+    else:
+        ast.append(node)
+        logger.debug("[Parser] Adicionado VAR_DECL à AST (fora de módulo): %s", str(node))
+
+
+def handle_port(node, ast, module_stack, block_stack):
+    if block_stack:
+        block_stack[-1].children.append(node)
+        logger.debug("[Parser] Aninhado PORT no bloco ativo: %s", str(node))
+    elif module_stack:
+        module_stack[-1].children.append(node)
+        logger.debug("[Parser] Aninhado PORT no MODULE: %s", str(node))
+    else:
+        ast.append(node)
+        logger.debug("[Parser] Adicionado PORT à AST (fora de módulo): %s", str(node))
+
+
+def handle_initial_start(node, ast, module_stack, block_stack):
+    if block_stack:
+        block_stack[-1].children.append(node)
+        logger.debug("[Parser] Aninhado INITIAL_START no bloco ativo: %s", str(node))
+    elif module_stack:
+        module_stack[-1].children.append(node)
+        logger.debug("[Parser] Aninhado INITIAL_START no MODULE: %s", str(node))
+    else:
+        ast.append(node)
+        logger.debug("[Parser] Adicionado INITIAL_START à AST (fora de módulo): %s", str(node))
+
+
+def handle_block_start(node, ast, module_stack, block_stack):
+    # Já é tratado pelo parser específico, se necessário.
+    pass
+
+
+def handle_block_end(node, ast, module_stack, block_stack):
+    if block_stack:
+        block_stack.pop()
+        logger.debug("[Parser] Bloco fechado, block_stack: %s", [str(n) for n in block_stack])
+
+
+# Dicionário para despacho das funções de encaixe
+ATTACH_HANDLERS = {
+    'MODULE_START': handle_module_start,
+    'MODULE_END': handle_module_end,
+    'ALWAYS_POSEDGE': handle_always_posedge,
+    'IF': handle_if,
+    'ELSE': handle_else,
+    'ASSIGN': handle_assign_print,
+    'PRINT': handle_assign_print,
+    'VAR_DECL': handle_var_decl,
+    'PORT': handle_port,
+    'INITIAL_START': handle_initial_start,
+    'BLOCK_START': handle_block_start,
+    'BLOCK_END': handle_block_end,
+}
+
+
+def attach_node(token_type, node, ast, module_stack, block_stack):
+    handler = ATTACH_HANDLERS.get(token_type)
+    if handler:
+        handler(node, ast, module_stack, block_stack)
+    else:
+        if block_stack:
+            block_stack[-1].children.append(node)
+            logger.debug("[Parser] Aninhado no bloco ativo: %s", str(node))
+        elif module_stack:
+            module_stack[-1].children.append(node)
+            logger.debug("[Parser] Aninhado no MODULE: %s", str(node))
+        else:
+            ast.append(node)
+            logger.debug("[Parser] Adicionado à AST (fora de módulo): %s", str(node))
+
+
 def syslog_parse(tokens):
     logger.debug("[Parser][syslog_parse] Iniciando parsing dos tokens: %s", tokens)
     ast = []
@@ -34,57 +165,11 @@ def syslog_parse(tokens):
         token_type = token['type']
         parser_func = PARSER_DISPATCH.get(token_type)
         if parser_func:
-            node = parser_func(token, module_stack if token_type in ('MODULE_START', 'MODULE_END') else block_stack)
+            # Usa module_stack para MODULE_START e MODULE_END; caso contrário, usa block_stack.
+            context = module_stack if token_type in ('MODULE_START', 'MODULE_END') else block_stack
+            node = parser_func(token, context)
             if node:
-                if token_type == 'MODULE_START':
-                    ast.append(node)
-                    module_stack.append(node)
-                    logger.debug("[Parser] Adicionado MODULE à AST: %s", str(node))
-                elif token_type == 'MODULE_END':
-                    if module_stack:
-                        module_stack.pop()
-                    if block_stack:
-                        block_stack.clear()
-                    logger.debug("[Parser] MODULE finalizado, AST atual: %s", [str(n) for n in ast])
-                elif token_type == 'ALWAYS_POSEDGE':
-                    if module_stack:
-                        module_stack[-1].children.append(node)
-                        block_stack.append(node)  # Inicia o bloco
-                        logger.debug("[Parser] Aninhado ALWAYS_POSEDGE no MODULE e block_stack: %s", str(node))
-                elif token_type == 'IF':
-                    if block_stack:
-                        block_stack[-1].children.append(node)
-                        block_stack.append(node)  # Empilha o IF como bloco ativo
-                        logger.debug("[Parser] Aninhado IF no bloco ativo e empilhado: %s", str(node))
-                elif token_type == 'ELSE':
-                    if block_stack and block_stack[-1].type == 'IF':
-                        block_stack.pop()  # Fecha o IF
-                    if block_stack:
-                        block_stack[-1].children.append(node)
-                        block_stack.append(node)  # Empilha o ELSE como bloco ativo
-                        logger.debug("[Parser] Aninhado ELSE no bloco ativo e empilhado: %s", str(node))
-                elif token_type in ('ASSIGN', 'PRINT'):
-                    if block_stack:
-                        block_stack[-1].children.append(node)
-                        logger.debug("[Parser] Aninhado %s no bloco ativo: %s", token_type, str(node))
-                    elif module_stack:
-                        module_stack[-1].children.append(node)
-                        logger.debug("[Parser] Aninhado %s no MODULE: %s", token_type, str(node))
-                elif token_type == 'BLOCK_START':
-                    pass  # Já tratado pelo parser específico, se necessário
-                elif token_type == 'BLOCK_END':
-                    if block_stack:
-                        block_stack.pop()
-                        logger.debug("[Parser] Bloco fechado, block_stack: %s", [str(n) for n in block_stack])
-                elif block_stack:
-                    block_stack[-1].children.append(node)
-                    logger.debug("[Parser] Aninhado no bloco ativo: %s", str(node))
-                elif module_stack:
-                    module_stack[-1].children.append(node)
-                    logger.debug("[Parser] Aninhado no MODULE: %s", str(node))
-                else:
-                    ast.append(node)
-                    logger.debug("[Parser] Adicionado à AST (fora de módulo): %s", str(node))
+                attach_node(token_type, node, ast, module_stack, block_stack)
         else:
             logger.warning("[Parser][syslog_parse] Tipo de token não suportado: %s", token_type)
 
